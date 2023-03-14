8b8860d433028caced6993be3bcd944380d57192
Shaily S <68847710+shaily23@users.noreply.github.com>
2023-03-14 12:00:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import PyPDF2
 
 b = PyPDF2.PdfReader('a.pdf')
-str = ""
+print(len(b.pages))
+page = b.pages[0]
 
 
-for i in range(0,20):
-    str += b.pages[i].extract_text()
+for i in range(len(b.pages)):
+    str += b.pages[i]
+    print(page.extract_text())
 
 
 with open("text.txt", "w", encoding='utf-8') as f:   
-    f.write(str.split(".")[1])
-print(str.split(".")[1])
+    f.write(page.split(".")[1])
